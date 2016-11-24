64fb36e1954e37abf316a3ff10dc674ebeb082ca
Ivan Muñoz Troyano <ivanmunozt@gmail.com>
2016-11-25 00:46:54 +0100

    Interacting with layout

--- app/src/main/java/com/example/imunoz/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/imunoz/myapplication/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.imunoz.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener { toast("Message: ${input.text}") }
     }
 }
